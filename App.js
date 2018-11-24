c329bdefa77b761c7775d7b58768674301620c07
Luke Rogerson <lukerogerson@aol.com>
2018-11-24 10:28:18 +0100

    Swap out react navigation package for older version due to errors

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>MAIN SCREEN</Text>
+        <Text>MAIN SCREEN!!!!!!!</Text>
       </View>
     );
   }
