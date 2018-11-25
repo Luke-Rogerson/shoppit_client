3dbc81cca375e17f88bfac2300bc7b99ea2d18c2
Luke Rogerson <lukerogerson@aol.com>
2018-11-25 18:49:53 +0100

    Disable back button and prevent user being able to swipe to go back

--- navigation/SignUpStack.js
+++ navigation/SignUpStack.js
@@ -4,8 +4,20 @@ import SignInScreen from '../screens/SignInScreen';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import ReadyScreen from '../screens/ReadyScreen';
 
-export default createStackNavigator({
-  SignInScreen,
-  CategoriesScreen,
-  ReadyScreen,
-});
+export default createStackNavigator(
+  {
+    SignInScreen,
+    CategoriesScreen,
+    ReadyScreen,
+  },
+  {
+    headerMode: 'float',
+    navigationOptions: {
+      headerStyle: { backgroundColor: '#E73536' },
+      title: 'TINDER FOR COOL SHIT',
+      headerTintColor: 'white',
+      headerLeft: null, // Disables back button
+      gesturesEnabled: false // Prevents user being able to swipe left to go back in stack
+    }
+  }
+);
