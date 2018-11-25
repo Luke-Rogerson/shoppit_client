0b538bc30dc70a79978615e0d11ba3b537fb8649
Luke Rogerson <lukerogerson@aol.com>
2018-11-26 00:07:20 +0100

    Add stacks for profile and friend views

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,10 +2,8 @@ import { createSwitchNavigator } from 'react-navigation';
 
 import SignUpStack from './SignUpStack';
 import DrawerNavigator from './DrawerNavigator';
-import MainViewsStack from './MainViewsStack';
 
 export default createSwitchNavigator({
-  SignUp: SignUpStack,
-  DrawerNav: DrawerNavigator,
-  MainViews: MainViewsStack,
+  SignUpStack,
+  DrawerNavigator
 });
