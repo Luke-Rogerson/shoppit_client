755169fab560f689dfb811f4d1ecd79323fd1911
Luke Rogerson <lukerogerson@aol.com>
2018-11-26 00:06:43 +0100

    Add link to profile page

--- screens/friendListScreen.js
+++ screens/friendListScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class FriendListScreen extends React.Component {
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>FRIEND LIST SCREEN</Text>
+        <Button
+          title="Friend #1"
+          onPress={() => navigate('ProfileScreen')}
+        />
       </View>
     );
   }
