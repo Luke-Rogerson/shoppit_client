f835b3230e7817a27ddfb6119b5107a08b7578a0
Luke Rogerson <lukerogerson@aol.com>
2018-11-25 18:51:12 +0100

    Remove navigationOptions setter

--- screens/signInScreen.js
+++ screens/signInScreen.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class SignInScreen extends React.Component {
-  static navigationOptions = () => ({
-    title: 'SignIn '
-  });
   render() {
     const {navigate} = this.props.navigation;
     return (
