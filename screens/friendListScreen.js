e0be895013bf5f5d9f7b0a7cdef42f16fc102460
Luke Rogerson <lukerogerson@aol.com>
2018-11-26 18:06:32 +0100

    Fetch user data from apiary and store in state

--- screens/friendListScreen.js
+++ screens/friendListScreen.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class FriendListScreen extends React.Component {
+
   render() {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>FRIEND LIST SCREEN</Text>
-        <Button
-          title="Friend #1"
-          onPress={() => navigate('ProfileScreen')}
-        />
+        <Button title="Friend #1" onPress={() => navigate('ProfileScreen')} />
       </View>
     );
   }
--- screens/profileScreen.js
+++ screens/profileScreen.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import mockdb from '../mockdb.json';
 
 export default class ProfileScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://private-e029e-wisher.apiary-mock.com/profile/me')
+      .then(res => res.json())
+      .then(data => this.setState({ userData: data }))
+      // eslint-disable-next-line no-console
+      .catch(error => console.error('Error:', error));
+  }
+
   render() {
+    const { userData } = this.state;
     const {navigate} = this.props.navigation;
+
+    if (!userData) return <Text>Loading...</Text>;
+
     return (
       <View style={styles.container}>
         <Text>PROFILE SCREEN</Text>
