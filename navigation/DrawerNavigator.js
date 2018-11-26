db28e144ee799e0b0866ad8ad8bebe9a30385470
Luke Rogerson <lukerogerson@aol.com>
2018-11-26 12:34:29 +0100

    Add icons to bottom tabs

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
+import { Entypo } from '@expo/vector-icons';
 
 import HomeStack from './HomeStack';
 import MyProfileStack from './MyProfileStack';
@@ -6,11 +8,32 @@ import FriendsStack from './FriendsStack';
 
 export default createBottomTabNavigator(
   {
-    HomeStack,
-    MyProfileStack,
-    FriendsStack
+    Home: {
+      screen: HomeStack,
+      navigationOptions: {
+        title: 'Home',
+        tabBarIcon:
+          <Entypo name='home' size={25} color='lightgrey' style={{paddingTop: 5}} />
+      }
+    },
+    MyProfile: {
+      screen: MyProfileStack,
+      navigationOptions: {
+        title: 'My Profile',
+        tabBarIcon:
+          <Entypo name='user' size={25} color='lightgrey' style={{paddingTop: 5}} />
+      }
+    },
+    Friends: {
+      screen: FriendsStack,
+      navigationOptions: {
+        title: 'Friends',
+        tabBarIcon:
+          <Entypo name='users' size={25} color='lightgrey' style={{paddingTop: 5}} />
+      }
+    }
   },
   {
-    initialRouteName: 'HomeStack'
+    initialRouteName: 'Home'
   }
 );
