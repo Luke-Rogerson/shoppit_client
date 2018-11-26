03d439317bc40db6fbb12b67ed99aba7f38f35a7
Luke Rogerson <lukerogerson@aol.com>
2018-11-26 18:28:58 +0100

    Show username and profile picture in profileScreen

--- screens/profileScreen.js
+++ screens/profileScreen.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import mockdb from '../mockdb.json';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
 
 export default class ProfileScreen extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      userData: null,
+      userData: null
     };
   }
 
@@ -22,13 +21,25 @@ export default class ProfileScreen extends React.Component {
 
   render() {
     const { userData } = this.state;
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
 
     if (!userData) return <Text>Loading...</Text>;
-
     return (
       <View style={styles.container}>
-        <Text>PROFILE SCREEN</Text>
+        <Text>
+          {this.state.userData.first_name} {this.state.userData.last_name}
+        </Text>
+        <Image
+          source={{ uri: this.state.userData.avatar_url }}
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            borderWidth: 1,
+            borderColor: 'black',
+            margin: 5
+          }}
+        />
         <Button
           title="Imagine this is an item"
           onPress={() => navigate('ItemDetailScreen')}
@@ -42,7 +53,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'center'
   }
 });
