c548eea61fc9f1bf2ac4d35fbeabdf40e9d77a39
Luke Rogerson <lukerogerson@aol.com>
2018-11-27 13:22:31 +0100

    Create basic auth, entities, pages reducers and combine

---
+++ reducers/entities.js
@@ -0,0 +1,14 @@
+const defaultState = {
+  users: {},
+  categories: {},
+  items: {}
+};
+
+const entities = (state = defaultState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default entities;
---
+++ reducers/pages.js
@@ -0,0 +1,17 @@
+const defaultState = {
+  pages: {
+    friends: {
+      friendsList: [],
+      isLoading: false
+    }
+  }
+};
+
+const pages = (state = defaultState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default pages;
