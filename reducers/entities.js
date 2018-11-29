71d493ac0a68a1ccf91d68b9865c287450d2a390
Luke Rogerson <lukerogerson@aol.com>
2018-11-29 16:40:37 +0100

    Remove recommended items and liked items and make one item store

--- reducers/entities.js
+++ reducers/entities.js
@@ -2,8 +2,7 @@ const defaultState = {
   currentUser: {},
   friends: {},
   categories: {},
-  recommendedItems: {},
-  likedItems: {}
+  items: {}
 };
 
 const entities = (state = defaultState, action) => {
@@ -22,17 +21,13 @@ const entities = (state = defaultState, action) => {
       ...state.friends,
       ...entities.friends
     },
-    recommendedItems: {
-      ...state.recommendedItems,
-      ...entities.recommendedItems
-    },
     categories: {
       ...state.categories,
       ...entities.categories
     },
-    likedItems: {
-      ...state.likedItems,
-      ...entities.likedItems
+    items: {
+      ...state.items,
+      ...entities.items
     }
   };
 };
