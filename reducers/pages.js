5111181b321c8d2203f6b705c3bc9a225bb12bb7
Luke Rogerson <lukerogerson@aol.com>
2018-11-29 16:41:24 +0100

    Add default category and selected category state

--- reducers/pages.js
+++ reducers/pages.js
@@ -5,16 +5,20 @@ const defaultState = {
       isLoading: false
     },
     categoriesPage: {
-      categories: [23, 78, 873],
-      selectedCategories: [7, 6, 8],
-      loading: false,
-      // GET_CATEGORIES_FAILURE
+      // categories: [23, 78, 873],
+      // selectedCategories: [7, 6, 8],
+      categories: [],
+      selectedCategories: [],
+      loading: false
     }
   }
 };
 
 const pages = (state = defaultState, action) => {
   switch (action.type) {
+  case 'SELECT_CATEGORY':
+    return { ...state, };
+
   default:
     return state;
   }
